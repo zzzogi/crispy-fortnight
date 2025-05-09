9243fb332aded889716c4a8f0c2c08fa0bde2f52
seta-quyenbui <74925273+zzzogi@users.noreply.github.com>
2025-05-09 23:33:04 +0700

    nice 20

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import AuthProvider from "../providers/AuthProvider";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Đăng nhập",
@@ -15,7 +16,15 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <body>
-        <AuthProvider>{children}</AuthProvider>
+        <Suspense
+          fallback={
+            <div className="flex min-h-screen items-center justify-center">
+              Đang tải...
+            </div>
+          }
+        >
+          <AuthProvider>{children}</AuthProvider>
+        </Suspense>
       </body>
     </html>
   );
